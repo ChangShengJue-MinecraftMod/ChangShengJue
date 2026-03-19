bd6c1205c346cca0cde9e21119623332218d1ec4
hateful-human <103984002+HatefulHuman@users.noreply.github.com>
2026-03-19 21:05:09 +0800

    回退合并
    回退合并

--- src/main/java/com/shengchanshe/chang_sheng_jue/event/CSJEventBusEvents.java
+++ src/main/java/com/shengchanshe/chang_sheng_jue/event/CSJEventBusEvents.java
@@ -4,7 +4,6 @@ import com.shengchanshe.chang_sheng_jue.ChangShengJue;
 import com.shengchanshe.chang_sheng_jue.entity.ChangShengJueEntity;
 import com.shengchanshe.chang_sheng_jue.entity.combat.stakes.StakesEntity;
 import com.shengchanshe.chang_sheng_jue.entity.custom.butterfly.Butterfly;
-import com.shengchanshe.chang_sheng_jue.entity.custom.checkin.CheckInNPC;
 import com.shengchanshe.chang_sheng_jue.entity.custom.cicada.Cicada;
 import com.shengchanshe.chang_sheng_jue.entity.custom.crane.Crane;
 import com.shengchanshe.chang_sheng_jue.entity.custom.croc.Croc;
@@ -81,8 +80,6 @@ public class CSJEventBusEvents {
         event.put(ChangShengJueEntity.SWORD_MING_XIA.get(), AbstractMingXia.setAttributes());
         event.put(ChangShengJueEntity.KNIFE_MING_XIA.get(), AbstractMingXia.setAttributes());
         event.put(ChangShengJueEntity.FIST_MING_XIA.get(), AbstractMingXia.setAttributes());
-
-        event.put(ChangShengJueEntity.CHECKIN_NPC.get(), CheckInNPC.setAttributes());
 
         event.put(ChangShengJueEntity.STAKES.get(), StakesEntity.setAttributes());
     }
